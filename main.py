b4df1063a8dcecda71123f17872fb94920ad10f6
Nguyễn Huy Hoàng <85247788+hoangnguyen1104@users.noreply.github.com>
2024-10-25 14:40:19 +0700

    init project

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    name = "okoko"
+    return {"message": f"Hello ffff{name}"}
